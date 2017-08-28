5b0a583f2a434d65203fb68910e414b4fe982ef0
Carlos Alexandre <01.lexandre@gmail.com>
2017-08-28 10:09:36 -0300

    COREAUTH-125
    SyncLang mescla varias translate e uma apenas.
    Mas vc tem que copiar o obj inteiro quando for rescrever o translate.

--- src/app/plugins/translate/index.js
+++ src/app/plugins/translate/index.js
@@ -8,25 +8,26 @@ import store from '@plugins/store'
 import vuexI18n from 'vuex-i18n'
 
 // load translates js
-import { translates as messages } from '@/app/domains'
 import { translates as transAuth } from '../../../../package/vuejs-auth/src/index'
+import { translates as messages } from '@/app/domains'
 
-// initialize the internationalization plugin on the vue instance. note that
-// the store must be passed to the plugin. the plugin will then generate some
-// helper functions for components (i.e. this.$i18n.set, this.$t) and on the vue
-// instance (i.e. Vue.i18n.set).
 Vue.use(vuexI18n.plugin, store)
 
-// add translations directly to the application
-Vue.i18n.add('en', messages.en)
-Vue.i18n.add('ptBR', messages.ptBR)
-Vue.i18n.add('en', transAuth.en)
-Vue.i18n.add('ptBR', transAuth.ptBR)
+let syncLang = (lang) => {
+  return {
+    ...transAuth[lang],
+    ...messages[lang]
+  }
+}
 
-// set the start locale to use
+Vue.i18n.add('en', syncLang('en'))
+Vue.i18n.add('ptBR', syncLang('ptBR'))
+//
+// Vue.i18n.add('en', transAuth.en)
+// Vue.i18n.add('ptBR', transAuth.ptBR)
+
 Vue.i18n.set(process.env.LOCALE)
 
-// set a fallback locale if translation for current locale does not exist
 Vue.i18n.fallback('ptBR')
 
 export default Vue.i18n
